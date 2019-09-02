1d577ac829738dd3608a2f7ee19ea5b13e7db41f
Alexis <apumayallag@gmail.com>
2019-09-02 15:53:39 -0500

    login

--- app/src/main/java/com/farenet/descuentos/config/Constante.java
+++ app/src/main/java/com/farenet/descuentos/config/Constante.java
@@ -16,9 +16,6 @@ public class Constante {
     //key shpf
     public static final String TOKEN = "TOKEN";
 
-    public static final String user = "apumayalla";
-    public static final String password = "123qwe";
-
     public static LoginRepository getLoginRespository(){
         return RetrofitMaestro.getMaestros(API_URL).create(LoginRepository.class);
     }
