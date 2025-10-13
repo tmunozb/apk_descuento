b3ce0068f1a610de8c65f9824ec97e643c510e86
tmunozb <u201210369@upc.edu.pe>
2025-10-13 11:42:01 -0500

    Descuentos v.13.1.10.25

--- app/src/main/java/com/farenet/descuentos/ui/comercialhost/ComercialHostActivity.java
+++ app/src/main/java/com/farenet/descuentos/ui/comercialhost/ComercialHostActivity.java
@@ -1,5 +1,6 @@
 package com.farenet.descuentos.ui.comercialhost;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -7,10 +8,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager2.widget.ViewPager2;
 
+import com.farenet.descuentos.Core.Storage.SessionManager;
 import com.farenet.descuentos.R;
+import com.farenet.descuentos.ui.auth.LoginActivity;
 import com.farenet.descuentos.ui.comercialhost.adapter.TabsAdapter;
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class ComercialHostActivity extends AppCompatActivity {
@@ -18,10 +24,14 @@ public class ComercialHostActivity extends AppCompatActivity {
     private ViewPager2 pager;
     private BottomNavigationView bottom;
 
+    private SessionManager session;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comercial_host);
+
+        session = new SessionManager(this);
 
         MaterialToolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -67,6 +77,30 @@ public class ComercialHostActivity extends AppCompatActivity {
 
         // Tab inicial
         bottom.setSelectedItemId(R.id.tab_home);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_home_comercial, menu); // <-- infla SIEMPRE aquí
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.nav_logout) {
+            // Limpia sesión y caches que uses
+            session.clear();
+            // DashboardCache.clear(); BolsaCache.clear(); // si tienes estos helpers
+
+            Intent i = new Intent(this, LoginActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
+                    | Intent.FLAG_ACTIVITY_NEW_TASK
+                    | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(i);
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void setCurrent(int index) {
